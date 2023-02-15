54b2bb0aa7e2db1594d18cd46c978db63ab2f94f
IlyaKoch <kochetkov6363@gmail.com>
2023-02-15 21:46:46 +0400

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Проект c изучением Java Core
+### Technology stack:
+- Java
+- Gradle
+- Junit5
+- Selenide
---
+++ src/main/java/com/kochetkov/Main.java
@@ -0,0 +1,7 @@
+package com.kochetkov;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
